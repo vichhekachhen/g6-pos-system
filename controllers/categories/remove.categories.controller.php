2407068163933b5f4d64ac83f1ca98fcc810be62
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-02-16 11:05:23 +0700

    [feat]delete-category #GPS19

---
+++ controllers/categories/remove.categories.controller.php
@@ -0,0 +1,9 @@
+<?php
+require('../../database/database.php');
+require('../../models/category.model.php');
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $isDeleted =  deletePost($id);
+}
+header('location:/categories');
