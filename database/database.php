07fad5d3e6798f0ed50194e27dacaa412a5573a9
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-02-16 10:50:31 +0700

    view code

--- database/database.php
+++ database/database.php
@@ -5,5 +5,5 @@ $database = "demo_db";
 $username = "root";
 $password = "";
 
-// $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
-// $connection = new PDO($dsn, $username, $password);
+$dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+$connection = new PDO($dsn, $username, $password);
