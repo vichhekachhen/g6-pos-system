0dae40d72d193231c6d077201561a934c83c6a5c
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-02-21 08:49:49 +0700

    [imp]create-category#GPS-17

--- controllers/categories/insert_categories.php
+++ controllers/categories/insert_categories.php
@@ -3,11 +3,11 @@ require('../../database/database.php');
 require('../../models/category.model.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
- 
-    $categoryName = $_POST['name'];
-    $description = $_POST['description'];
-
-    $iscreate = createCategory( $categoryName, $description);
+    if ($_POST["name"] !== '' && $_POST['description'] !== '') {
+        $categoryName = $_POST['name'];
+        $description = $_POST['description'];
+        $iscreate = createCategory($categoryName, $description);
+    }
     if ($iscreate) {
         header('location:/categories');
     } else {
