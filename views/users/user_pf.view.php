1881e0e263532de77003676a5c0b5ac806b09529
vichhekachhen <141004976+vichhekachhen@users.noreply.github.com>
2024-02-26 08:56:02 +0700

    [imp] update interface and fix code #GPS-69

--- views/users/user_pf.view.php
+++ views/users/user_pf.view.php
@@ -4,7 +4,7 @@ require "models/user.model.php";
 ?>
 <?php
 $id =  $_GET['id'];
-// $user = viewUser($id);
+$user = viewUser($id);
 ?>
 
 <div class="container-fluid">
@@ -15,7 +15,7 @@ $id =  $_GET['id'];
         <div class="col-sm-2"><a href="/users" class="pull-right"><img class="img-circle img-responsive" src=""></a></div>
     </div>
     <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-4 mt-4">
             <div class="text-center">
                 <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
                 <h4 class="m-2"><?= $user['user_name'] ?></h4>
@@ -24,39 +24,42 @@ $id =  $_GET['id'];
         <div class="col-sm-6">
             <div class="tab-content">
                 <div class="tab-pane active" id="home">
-                    <form class="form" action="##" method="post" id="profile">
+                    <form class="form mt-5" action="##" method="post" id="profile">
                         <div class="form-row mt-4 mb-3">
                             <div class="col">
-                                <strong>User ID</strong><input type="text" class="form-control" value="<?= $user['user_id'] ?>" name="user_id">
+                                <strong>User ID : </strong><?= $user['user_id'] ?>
                             </div>
                             <div class="col">
-                                <strong>User Name</strong><input type="text" class="form-control" value="<?= $user['user_name'] ?>" name="user_name">
+                                <strong>User Name : </strong><?= $user['user_name'] ?>
                             </div>
                         </div>
                         <div class="form-row mt-4 mb-3">
                             <div class="col">
-                                <strong>Phone</strong><input type="text" class="form-control" value="<?= $user['phone'] ?>" name="phone">
+                                <strong>Phone : </strong><?= $user['phone'] ?>
                             </div>
                             <div class="col">
-                                <strong>Email</strong><input type="text" class="form-control" value="<?= $user['email'] ?>" name="email">
+                                <strong>Email : </strong><?= $user['email'] ?>
                             </div>
                         </div>
                         <div class="form-row mt-4 mb-3">
                             <div class="col">
-                                <strong>City</strong><input type="text" class="form-control" value="<?= $user['city'] ?>" name="city">
+                                <strong>City : </strong><?= $user['city'] ?>
                             </div>
                             <div class="col">
-                                <strong>Country</strong><input type="text" class="form-control"value="<?= $user['country'] ?>" name="country">
+                                <strong>Country : </strong><?= $user['country'] ?>
                             </div>
                         </div>
                         <div class="form-row mt-4 mb-3">
                             <div class="col-6">
-                                <strong>Role</strong><input type="text" class="form-control" value="<?= $user['role'] ?>" name="role">
+                                <strong>Role : </strong><?= $user['role'] ?>
                             </div>
                         </div>
                         <div class="form-row d-flex justify-content-end mt-3">
                             <a href="/users" class="btn btn-primary mr-3">Back</a>
                         </div>
                     </form>
-
-                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
