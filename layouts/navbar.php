8ea8b558a723a087b787744e9a4640c1002e059a
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2024-03-02 22:45:02 +0700

    [ipm] fix code error #GPS-94

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,8 +1,8 @@
  <!-- Sidebar -->
  <?php
-    session_start();
-    $profile = $_SESSION["profile_image"];
-    $Role =   $_SESSION["role"];
+    // session_start();
+    // $profile = $_SESSION["profile_image"];
+    // $Role =   $_SESSION["role"];
     ?>
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
@@ -235,8 +235,8 @@
                  <!-- Nav Item - User Information -->
                  <li class="nav-item dropdown no-arrow">
                      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$Role?></span>
-                         <img class="img-profile rounded-circle" src="assets/profile_img/<?= $profile ?>">
+                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
+                         <img class="img-profile rounded-circle" src="assets/profile_img/">
                      </a>
                      <!-- Dropdown - User Information -->
                      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
