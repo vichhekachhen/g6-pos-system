b79a06407e54b301ad3fe93061890b54dbdf709c
BUNNARITHHHHH <bunnarith.phoeurn@student.passerellesnumeriques.org>
2024-03-04 08:28:15 +0700

    [imp] fix code error #GPS-94

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -234,11 +234,7 @@
                  <!-- Nav Item - User Information -->
                  <li class="nav-item dropdown no-arrow">
                      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-<<<<<<< HEAD
-                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
-=======
                          <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $username ?></span>
->>>>>>> origin/create-profile
                          <img class="img-profile rounded-circle" src="assets/profile_img/<?= $profile ?>">
                      </a>
                      <!-- Dropdown - User Information -->
