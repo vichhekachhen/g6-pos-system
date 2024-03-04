c8c98ec0f88193fec1b90483b34fc07f14a702ae
SAVORN.VORN <SAVORN.VORN@PNC.LAN>
2024-03-04 10:41:22 +0700

    [feat] create page and click order show table customer #GPS-27

--- controllers/items/remove_item.controller.php
+++ controllers/items/remove_item.controller.php
@@ -1,16 +1,22 @@
-
-
 <?php
 
     require_once "../../database/database.php";
     require_once "../../models/item.model.php";
- // function delete item from database
-    $id = $_GET['id'];
-    if (isset($id)){
-
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
         deleteItem($id);
-
+    
+        // Show a confirmation dialog using JavaScript
+        echo "<script>
+                if(confirm('Are you sure you want to delete this item?')) {
+                    window.location.href = '/items?id= deleteItem($id)';
+                    window.location.href = '/items'; 
+                }
+              </script>";
     }
-    header('Location:/items');
+    else {
+        header('Location: /items');
+    
+    }
 
-// ?>
+?>
