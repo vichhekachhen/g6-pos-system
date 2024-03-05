db54742dd7f96288d7f8eb98bb68c7684f4bf68c
SAVORN.VORN <SAVORN.VORN@PNC.LAN>
2024-03-05 09:51:57 +0700

    [ump] update code function #GPS-27

--- views/customers/form_create_customer.view.php
+++ views/customers/form_create_customer.view.php
@@ -1,5 +1,6 @@
 <?php
 require  "models/item.model.php";
+require "models/customer.model.php";
 $getAllitem = getAllItems();
 ?>
 
@@ -200,24 +201,7 @@ $getAllitem = getAllItems();
   </main>
 
   <script>
-    function showProductDetails(name, price) {
-      // Get the table body
-      var tableBody = document.getElementById("productDetailsBody");
-      // Create a new row
-      var row = tableBody.insertRow();
-      // Insert cells for the row
-      var productNameCell = row.insertCell(0);
-      var quantityCell = row.insertCell(1);
-      var priceCell = row.insertCell(2);
-      var actionCell = row.insertCell(3);
-      // Set the cell values
-      productNameCell.innerHTML = name;
-      quantityCell.innerHTML = "<input type='number' min='1' value='1'>";
-      priceCell.innerHTML = "$" + price;
-      actionCell.innerHTML = "<button class='btn btn-danger'>Remove</button>";
-      // Show the product details table
-      document.getElementById("productDetails").style.display = "block";
-    }
+   showProductDetails(name, price)
   </script>
 
   <script src="https://cdn.jsdelivr.net"></script>
