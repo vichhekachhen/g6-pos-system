fcd6cdd3012905d9cf5d4ff26d1e49ba1c502bbb
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-03-22 20:33:57 +0700

    [imp] print-receipt #GPS-31

--- vendor/print/print.js
+++ vendor/print/print.js
@@ -1,4 +1,8 @@
 
-function Getprint() {
-    window.print();
+function printData() {
+    var divToPrint = document.getElementById("printTable");
+    newWin = window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
 }
--- views/reports/report.view.php
+++ views/reports/report.view.php
@@ -1,11 +1,10 @@
-
 <script src="../../vendor/print/print.js" defer></script>
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
     <div>
-        <button type="button" class="btn btn-primary" onclick=" Getprint()">Print</button>
+        <button type="button" id="printTable" class="btn btn-primary" onclick=" printData()">Print</button>
 
     </div>
 
